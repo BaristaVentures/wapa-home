3366cfb4709845cd6cefd7b845a1c10adcfd3362
David Alzate <david@nullindustries.co>
2015-07-30 20:55:10 -0500

    first commit

---
+++ javascripts/global.js
@@ -0,0 +1,14 @@
+$(window).load(function() {
+  $('.bxslider').bxSlider({
+    slideWidth: 212,
+    slideMargin: 10,
+    maxSlides: 7,
+    speed: 1000,
+    responsive: true,
+    controls: false,
+    infiniteLoop: true,
+    pager: false,
+    auto: true,
+    moveSlides: 1
+  });
+});
