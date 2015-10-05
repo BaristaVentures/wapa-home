032c5de9895878c67c1344705dda3021c6fc95c6
Alejandra Valencia <Alejandra@MacBook-Pro-de-Alejandra.local>
2015-10-04 23:46:21 -0500

    Add function js-do-book

--- javascripts/global.js
+++ javascripts/global.js
@@ -11,4 +11,36 @@ $(window).load(function() {
     auto: true,
     moveSlides: 1
   });
+
+  $(".js-button-input").on("click", function(){
+    $(this).addClass("btn-reserve--width");
+    setTimeout(function(){
+      $(".js-button-input").fadeOut();
+      $(".js-box--input").fadeIn();
+      $(".js-input-email").focus();
+    }, 300);
+  });
+
+  $(".js-do-booknow").on("click", function(){
+    var emailVal = $(".js-input-email").val();
+    if(validateEmail(emailVal)){
+      Parse.initialize("1xzWcLBL9xXTP4s3XpdNYChaLP5MjZ7QPDNUiuDe", "co9aMd2V4V7DXeewbm9o27DRxES0NQup4iDWiWR1");
+      Parse.Cloud.run('sendReservation', { email: emailVal }, {
+        success: function(result) {
+          alert(result);
+        },
+        error: function(error) {
+          alert(error);
+        }
+      });
+    }else{
+      alert("Bad email, please type it again");
+    }
+
+  });
+
+  function validateEmail(email) {
+    var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    return re.test(email);
+  }
 });
